1358428a30273b61af321912c0457b54d5c3d2cf
Vishesh Dhawan <vishesh.dh1@gmail.com>
2025-12-22 10:58:08 +0530

    refactors

--- src/com/dsa/linkedlist/LinkedListUse.java
+++ src/com/dsa/linkedlist/LinkedListUse.java
@@ -9,15 +9,10 @@ public class LinkedListUse {
 
     // utils contains functions to perform basic operations on LinkedList
     private static final LinkedListUtils<Integer> utils = new LinkedListUtils<>();
+    private static final LinkedList<Integer> ll = new LinkedList<>();
 
-    public static void main(String[] args) {
-
-        LinkedList<Integer> ll = new LinkedList<>();
-        Node<Integer> head = ll.buildLLFromArray(SystemUtils.takeIntegerArrayInput("Enter array values: "));
-
-        // both are valid ways to print the linked list
-        ll.print();
-        utils.printLL(head);
+    private static void printNodeAtIndex() {
+        Node<Integer> head = ll.getHead();
 
         // both are valid ways to get a node at any index
         int index = SystemUtils.takeIntInput("Enter index: ");
@@ -26,5 +21,27 @@ public class LinkedListUse {
 
         System.out.println("Both nodes are same : " + (node1 == node2));
         System.out.println("data : " + (node1 == null ? null : node1.getData()));
+    }
+
+    public static void main(String[] args) {
+
+        // LinkedList<Integer> ll = new LinkedList<>();
+        Node<Integer> head = ll.buildLLFromArray(SystemUtils.takeIntegerArrayInput("Enter array values: "));
+
+        // both are valid ways to print the linked list
+        ll.print();
+        utils.printLL(head);
+
+        // printNodeAtIndex();
+
+        // both are valid ways to insert a node at any index
+        head = ll.insert(99, 1);
+        ll.print();
+
+        // calling utils.insert doesn't update the head pointer inside ll
+        // we have to call the ll.setHead manually
+        head = utils.insert(head, 98, 0);
+        ll.setHead(head);
+        utils.printLL(head); // or ll.print()
     }
 }
