8c7e59c22f8bb32e0874dbfd31ed578a22a8398c
visheshdvn <vishesh.dh1@gmail.com>
2026-05-11 10:25:14 +0530

    Heal Sort implemented

---
+++ src/com/dsaj/concepts/dsa/sortingalgorithms/InPlaceHeapSort.java
@@ -0,0 +1,55 @@
+package com.dsaj.concepts.dsa.sortingalgorithms;
+
+import com.dsaj.concepts.dsa.utils.ArrayUtils;
+import com.dsaj.concepts.dsa.utils.SystemUtils;
+
+public class InPlaceHeapSort {
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // One by one extract elements from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Call heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+    
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i; // Initialize largest as root
+        int left = 2 * i + 1; // left = 2*i + 1
+        int right = 2 * i + 2; // right = 2*i + 2
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = SystemUtils.takeIntArrayInput("Enter the elements of the array: ");
+        heapSort(arr);
+        System.out.println("Sorted array: ");
+        ArrayUtils.printIntArray(arr);
+    }
+}
