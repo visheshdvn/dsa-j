40cb95ff6526713ce3579536205b38d7cb3991b6
visheshdvn <vishesh.dh1@gmail.com>
2026-05-24 12:07:48 +0530

    LC347 solved

---
+++ src/com/dsaj/problems/leetcode/arrays/LC347TopKFrequentElement.java
@@ -0,0 +1,56 @@
+package com.dsaj.problems.leetcode.arrays;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.dsaj.concepts.dsa.utils.ArrayUtils;
+import com.dsaj.concepts.dsa.utils.SystemUtils;
+
+class LC347Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        List<Integer>[] buckets = new List[nums.length+1];
+        Map<Integer, Integer> frequencies = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            frequencies.put(nums[i], frequencies.getOrDefault(nums[i], 0)+1);
+        }
+
+        for (Integer num : frequencies.keySet()) {
+            Integer frequency = frequencies.get(num);
+
+            if(buckets[frequency] == null) {
+                buckets[frequency] = new ArrayList<>();
+            }
+
+            buckets[frequency].add(num);
+        }
+
+        int[] res = new int[k];
+        int added = 0;
+        for (int i = buckets.length-1; i >= 0 && added < k; i--) {
+            if(buckets[i] == null) continue;
+
+            for (Integer num: buckets[i]) {
+                res[added++] = num;
+            }
+        }
+
+        return res;
+    }
+}
+
+public class LC347TopKFrequentElement {
+
+    public static void main(String[] args) {
+        LC347Solution solution = new LC347Solution();
+
+        int[] arr = SystemUtils.takeIntArrayInput("Enter array elements: ");
+        int k = SystemUtils.takeIntInput("enter value of k: ");
+
+        int[] ans = solution.topKFrequent(arr, k);
+
+        ArrayUtils.printIntArray(ans);
+    }
+}
