65aafdf8fe541027649320bb0e01f9e79bb5f8d8
visheshdvn <vishesh.dh1@gmail.com>
2026-06-22 01:34:30 +0530

    LC1971 solved

--- src/com/dsaj/problems/leetcode/graph/LC1971FindIfPathExists.java
+++ src/com/dsaj/problems/leetcode/graph/LC1971FindIfPathExists.java
@@ -2,12 +2,18 @@ package com.dsaj.problems.leetcode.graph;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 import com.dsaj.concepts.dsa.utils.SystemUtils;
 
-class LC1971FindIfPathExistsSolutionNaive {
+interface LC1971FindIfPathExistsSolutionInterface {
+    boolean validPath(int n, int[][] edges, int source, int destination);
+}
+
+class LC1971FindIfPathExistsAdjMatrixSolution implements LC1971FindIfPathExistsSolutionInterface {
 
     private void traverseFromSource(int[][] graph, int source, int destination, boolean[] visited) {
         int[] children = graph[source];
@@ -42,7 +48,7 @@ class LC1971FindIfPathExistsSolutionNaive {
     }
 }
 
-class LC1971FindIfPathExistsSolution {
+class LC1971FindIfPathExistsAdjListDFSSolution implements LC1971FindIfPathExistsSolutionInterface {
 
     private void traverseFromSource(Map<Integer, List<Integer>> graph, int source, int destination, boolean[] visited) {
         visited[source] = true;
@@ -72,7 +78,7 @@ class LC1971FindIfPathExistsSolution {
             int dest = edge[1];
 
             List<Integer> children;
-            
+
             // straight edge
             children = graph.getOrDefault(src, new ArrayList<>());
             children.add(dest);
@@ -91,9 +97,77 @@ class LC1971FindIfPathExistsSolution {
     }
 }
 
+/*
+ * BFS solution is more efficient than DFS solution as it can find the
+ * destination node without traversing the entire graph. In DFS, we may end up
+ * traversing the entire graph before finding the destination node, especially
+ * if the destination node is located at a deeper level in the graph. On the
+ * other hand, BFS explores all nodes at the present depth level before moving
+ * on to the nodes at the next depth level, which allows it to find the
+ * destination node more quickly if it is located closer to the source node.
+ * 
+ * Time Complexity: O(V + E), where V is the number of vertices and E is the
+ * number of edges in the graph.
+ * Space Complexity: O(V), where V is the number of vertices in the graph, due
+ * to the visited array and the queue used for BFS.
+ * 
+ * changing the adjencency list implementation to use array of list instead of
+ * map further reduce the space complexity to O(V + E) as we can directly access
+ * the list of children for each node using its index in the array, eliminating
+ * the need for storing keys in a map.
+ */
+class LC1971FindIfPathExistsAdjListBFSSolution implements LC1971FindIfPathExistsSolutionInterface {
+
+    private boolean traverseFromSource(List<Integer>[] graph, int source, int destination,
+            boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(source);
+        visited[source] = true;
+
+        while (!queue.isEmpty()) {
+            if (visited[destination]) {
+                return true;
+            }
+
+            int node = queue.remove();
+            List<Integer> children = graph[node];
+
+            for (int i = 0; i < children.size(); i++) {
+                int child = children.get(i);
+                if (!visited[child]) {
+                    queue.add(child);
+                    visited[child] = true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] edge : edges) {
+            int src = edge[0];
+            int dest = edge[1];
+
+            graph[src].add(dest);
+            graph[dest].add(src);
+        }
+
+        // find if path exists
+        boolean[] visited = new boolean[n];
+        return traverseFromSource(graph, source, destination, visited);
+    }
+}
+
 public class LC1971FindIfPathExists {
     public static void main(String[] args) {
-        LC1971FindIfPathExistsSolution solution = new LC1971FindIfPathExistsSolution();
+        LC1971FindIfPathExistsSolutionInterface solution = new LC1971FindIfPathExistsAdjListBFSSolution();
 
         int n = SystemUtils.takeIntInput("Enter the number of nodes: ");
         int[][] edges = { { 0, 7 }, { 0, 8 }, { 6, 1 }, { 2, 0 }, { 0, 4 }, { 5, 8 }, { 4, 7 }, { 1, 3 }, { 3, 5 },
